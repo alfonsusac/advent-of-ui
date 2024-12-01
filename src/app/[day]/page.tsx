863d001e11d4330d6fa4ecc87b7546543f5112b9
Alfonsus Ardani <alfonsusac@gmail.com>
2024-12-01 17:41:50 +0700

    day ui

--- src/app/[day]/page.tsx
+++ src/app/[day]/page.tsx
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation";
+import Day1 from "../../content/2024/day1.mdx"
 
 export default async function DayPage(
   context: Promise<{
@@ -14,27 +15,23 @@ export default async function DayPage(
   }
 
   return (
-    <div className="mx-8">
+    <div className="mx-8 max-w-xl">
       <div className="flex gap-2 justify-between p-4">
-        <a href={"/"} className="font-bold tracking-tighter">Advent Of UI</a>
-        <div></div>
+        <a href={"/"} className="font-semibold tracking-tighter">
+          Advent Of UI
+        </a>
       </div>
 
-      <hr className="" />
-
-      <div className="p-8 pt-24 flex flex-row gap-8 justify-between">
+      <div className="p-8 pt-18 flex flex-row gap-8 justify-between">
         <div className="">
-          <h1 className="text-6xl font-bold tracking-tighter">Day {day}</h1>
+          <h1 className="text-5xl font-bold tracking-tighter">Day {day}</h1>
           <p className="">A UI challenge for the holiday season</p>
         </div>
-        <div>
-          23D:24H:59M:59S
-        </div>
       </div>
 
       <hr className="" />
 
-      
+      {day === 1 && <Day1 />}
     </div>
   );
 }
