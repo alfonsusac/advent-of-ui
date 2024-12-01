7f739af7766fa8d7b5682e81132f483785dcdae0
Alfonsus Ardani <alfonsusac@gmail.com>
2024-12-02 00:34:20 +0700

    link to repository

--- src/ui/footer.tsx
+++ src/ui/footer.tsx
@@ -5,9 +5,17 @@ export function Footer() {
       <a
         href="https://x.com/alfonsusac"
         target="_blank"
-        className="underline underline-offset-4 "
+        className="underline underline-offset-4 decoration-zinc-400"
       >
         @alfonsusac
+      </a>
+      {" | "}
+      <a
+        href="https://github.com/alfonsusac/advent-of-ui"
+        target="_blank"
+        className="underline underline-offset-4 decoration-zinc-400"
+      >
+        Repository
       </a>
     </div>
   );
